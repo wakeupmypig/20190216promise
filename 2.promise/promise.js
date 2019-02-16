75f780f4fcee69ebade383b809351dec297cfbe3
fandonglai <704072802@qq.com>
2019-02-16 11:13:09 +0800

    promise

---
+++ 2.promise/promise.js
@@ -0,0 +1,31 @@
+function Promise (executor){
+    // 在promise内部定义一个状态 当前promise的状态
+    let self = this;
+    self.value = undefined;
+    self.reason = undefined
+    self.status = 'pending'; // 默认promise的状态是pengding
+    function resolve(value){
+        if(self.status === 'pending'){
+            self.value = value;
+            self.status = 'resolved'; // 成功态
+        }
+       
+    }
+    function reject(reason){
+        if(self.status === 'pending'){
+            self.reason = reason;
+            self.status = 'rejected'; // 失败态
+        }
+    }
+    executor(resolve,reject);
+}
+Promise.prototype.then = function(onFulfilled,onRejected){
+    let self = this;
+    if(self.status === 'resolved'){
+        onFulfilled(self.value);
+    }
+    if(self.status === 'rejected'){
+        onRejected(self.reason);
+    }
+}
+module.exports = Promise;
--- note.md
+++ note.md
@@ -12,7 +12,9 @@ vue学习 vue全家桶。。。。
 
 ## promise 课程
 - callback （高阶函数） 一个函数返回一个函数 ／ 函数是参数  都是高阶函数 aop  
-- promise 用法 
+- promise 用法  Promise类 静态方法（Promise.xxx）  原型上的方法 (new Promise.xxx)
+    - (Promise.all Promise.resolve,Promise.reject,Promise.race)
+    - (then catch finally)
 - 封装promise
 - generator
 - async + await
@@ -20,3 +22,8 @@ vue学习 vue全家桶。。。。
 
 ## 会看地址
 http://video.zhufengpeixun.cn/ 进入班级在看视频
+
+## 上课方式 
+周一周三 晚上 8-10 点  周日全天 9.30－13:00  15:00-18:00
+第一期 优惠1000 参加过 js ／ node ＝> 优惠 1500  在线 8998  实体 10998
+时长三个月 （流原理）
